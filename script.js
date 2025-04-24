04292378c2d945bc756ca6d464e590aa90976359
AKASH PRAJAPATI <105411479+akashprajapati786@users.noreply.github.com>
2025-04-24 13:00:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function isSameType(value1, value2) {
   //your js code here
+	value1 = parseValue(value1);
+  value2 = parseValue(value2);
 	if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
   }
